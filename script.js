36c5487c5af34332a79ccab2bb181e40bf0ea4d3
tirtha8417 <70767842+tirtha8417@users.noreply.github.com>
2026-02-04 20:51:47 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const sections=document.querySelectorAll('.reveal');
-const observer=new IntersectionObserver(entries=>{
-entries.forEach(e=>{if(e.isIntersecting)e.target.classList.add('show')})
-},{threshold:.2});
-sections.forEach(s=>observer.observe(s));
